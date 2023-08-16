a5bd029c8ff69de0f94e2a7e3d500b455c80dd60
Huxley <huxdavi@gmail.com>
2023-08-16 12:46:04 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+define('INCLUDE_PATH','http://localhost/simple_smart/');
+
+
+//Conectar com banco de dados!
+define('HOST','localhost');
+define('USER','root');
+define('PASSWORD','');
+define('DATABASE','ss_system');
+
+//Constantes para o painel de controle
+define('NOME_EMPRESA','Simple Smart');
+
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Simple Smart</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <h2><a href="">Simple Smart</a></h2>
+            </div><!--logo-->
+            <div class="bg">
+                <div class="bg-img">
+                    <img src="img/bg.png">
+                </div><!--bg-img-->
+                <div class="bg-text">
+                    <h1>OBTENHA JÁ!<br/>O MELHOR SCRIPT<br/>DE PEDIDOS GRÁTIS</h1>
+                    <button><a href="">CLIQUE AQUI E OBTENHA JÁ!</a></button>
+                </div><!--bg-text-->
+            </div><!--bg-->
+        </div><!--container-->
+    </header>
+
+    <div class="primeira_chamada">
+        <div class="container">
+            <div class="pc_wrapper">
+                <div class="title">
+                    <h1>Especificações</h1>
+                    <x>Nosso script de sistema automatizado de pedidos com integrção via whatsapp</x>
+                </div><!--title-->
+                <div class="pc_text">
+                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <button><a href="">ADIQUIRA JÁ E AUTOMATIZE O SEU NEGÓCIO!</a></button>
+                </div><!--pc-text-->
+            </div><!--pc-wrapper-->
+        </div><!--container-->
+    </div><!--primeira-chamada-->
+</body>
+</html>
