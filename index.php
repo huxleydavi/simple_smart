f6058eb88c7794af7876e5352ec569a386572130
Huxley <huxdavi@gmail.com>
2023-08-21 15:26:45 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <title>Simple Smart</title>
 </head>
 <body>
-    <header>
+    <header id="head">
         <div class="container">
             <div class="logo">
                 <h2><a href="">Simple Smart</a></h2>
@@ -20,29 +20,86 @@
                 </div><!--bg-img-->
                 <div class="bg-text">
                     <h1>OBTENHA JÁ!<br/>O MELHOR SCRIPT<br/>DE PEDIDOS GRÁTIS</h1>
-                    <button><a href="">CLIQUE AQUI E OBTENHA JÁ!</a></button>
+                    <button><a href="#pc">CLIQUE AQUI E OBTENHA JÁ!</a></button>
                 </div><!--bg-text-->
             </div><!--bg-->
         </div><!--container-->
-    </header>
+    </header><!--header-->
 
-    <div class="primeira_chamada">
+    <div class="primeira_chamada" id="pc">
         <div class="container">
             <div class="pc_wrapper">
                 <div class="title">
                     <h1>Especificações</h1>
-                    <x>Nosso script de sistema automatizado de pedidos com integrção via whatsapp</x>
+                    <x>Nosso script de sistema automatizado de pedidos com integrção via whatsapp!</x>
                 </div><!--title-->
                 <div class="pc_text">
-                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
-                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
-                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
-                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
-                    <i class="fa fa-bars"><p>Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
-                    <button><a href="">ADIQUIRA JÁ E AUTOMATIZE O SEU NEGÓCIO!</a></button>
+                    <i class="fa fa-bars"><p>* Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>* Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>* Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>* Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <i class="fa fa-bars"><p>* Nosso script de sistema automatizado de pedidos com integrção via whatsapp.</p></i>
+                    <button><a href="#dp">ADIQUIRA JÁ E AUTOMATIZE O SEU NEGÓCIO!</a></button>
                 </div><!--pc-text-->
             </div><!--pc-wrapper-->
         </div><!--container-->
     </div><!--primeira-chamada-->
+
+    <div class="depoimentos" id="dp">
+        <div class="cotainer">
+            <div class="title">
+                <h1>Depoimentos</h1>
+                <x>Nossos clientes não usam outro sistema, eles usam o Simple Smart!</x>
+            </div><!--title-->
+            <div class="dp_wrapper">
+
+                    <div class="dp_single">
+                        <div class="dp_title">
+                            <div class="dp_img">
+                                <img src="img/perfil.png" alt="Perfil Depoimento">
+                            </div><!--dp_img-->
+                            <h4>Jhon Doe</h4>
+                        </div><!--dp_title-->
+                        <div class="dp_text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel convallis elit. Vivamus quis venenatis justo. Aliquam at finibus dui. Donec pretium iaculis nisl, a tempor eros porta vel. In aliquam nunc felis, non tempor massa cursus vitae</p>
+                        </div><!--dp_text-->
+                        <div class="dp_button">
+                            <a href="">VER O SITE</a>
+                        </div><!--dp_button-->
+                    </div><!--dp_single-->
+
+                    <div class="dp_single">
+                        <div class="dp_title">
+                            <div class="dp_img">
+                                <img src="img/perfil.png" alt="Perfil Depoimento">
+                            </div><!--dp_img-->
+                            <h4>Jhon Doe</h4>
+                        </div><!--dp_title-->
+                        <div class="dp_text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel convallis elit. Vivamus quis venenatis justo. Aliquam at finibus dui. Donec pretium iaculis nisl, a tempor eros porta vel. In aliquam nunc felis, non tempor massa cursus vitae</p>
+                        </div><!--dp_text-->
+                        <div class="dp_button">
+                            <a href="">VER O SITE</a>
+                        </div><!--dp_button-->
+                    </div><!--dp_single-->
+
+                    <div class="dp_single">
+                        <div class="dp_title">
+                            <div class="dp_img">
+                                <img src="img/perfil.png" alt="Perfil Depoimento">
+                            </div><!--dp_img-->
+                            <h4>Jhon Doe</h4>
+                        </div><!--dp_title-->
+                        <div class="dp_text">
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel convallis elit. Vivamus quis venenatis justo. Aliquam at finibus dui. Donec pretium iaculis nisl, a tempor eros porta vel. In aliquam nunc felis, non tempor massa cursus vitae</p>
+                        </div><!--dp_text-->
+                        <div class="dp_button">
+                            <a href="">VER O SITE</a>
+                        </div><!--dp_button-->
+                    </div><!--dp_single-->
+                </div><!--dp_wrapper-->
+                <div class="ab"><a href="#plans"><i class="fa fa-arrow-down"></i></a></div><!--ab-->
+        </div><!--container-->
+    </div><!--depoimentos-->
 </body>
 </html>
